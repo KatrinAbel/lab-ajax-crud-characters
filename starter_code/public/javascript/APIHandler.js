496cb1634407e82cbac98e0ae06128ad02d979eb
Katrin Abel <katrin@abel-essen.de>
2018-11-18 16:35:22 +0100

    -m "exercise iterations"

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,39 +4,43 @@ class APIHandler {
     // this.instance = axios.create({
     //   baseUrl: this.BASE_URL}
     // )
-
   }
 
   getFullList () {
     return axios.get(this.BASE_URL)
     .then( response => {
-      response.data
+      return response.data
     })
   }
 
   getOneRegister (id) {
     return axios.get(this.BASE_URL + id)
     .then( response => {
-      console.log('DATA',response.data);
-      response.data
+      return response.data
     })
   }
 
-  createOneRegister () {
+  createOneRegister (characterInfo) {
     return axios.post(this.BASE_URL, characterInfo)
     .then(response =>{
-      response.data})
-
+      return response.data})
     .catch(error => console.log(error))
-
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, characterInfo) {
+    return axios.patch(this.BASE_URL + id, characterInfo)
+    .then(response => {
+      return response.data})
+    .catch(error => console.log(error))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL + id)
+    .then (response => {
+      return "Character has been successfully deleted" 
+    })
+    .catch (error => {
+      return "Character not found"
+    })
   }
 }
-
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -5,24 +5,64 @@ $(document).ready(() => {
     charactersAPI.getFullList()
     .then(characters=>{
       console.log("debug click fetch all", characters)
+      addTableRows(characters)
       return characters
     })
-  
 }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.querySelector("input[name=character-id]").value
+    charactersAPI.getOneRegister(id)
+    .then(oneCharacter=>{
+      console.log("debug click fetch one", oneCharacter)
+      addTableRows(oneCharacter)
+      return oneCharacter
+    })
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    let id = document.querySelector("input[name=character-id-delete]").value
+    charactersAPI.deleteOneRegister(id)
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    let name = document.querySelector("#new-character-form input[name=name]").value    
+    let occupation = document.querySelector("#new-character-form input[name=occupation]").value
+    let cartoon = document.querySelector("#new-character-form input[name=cartoon]").checked
+    let weapon = document.querySelector("#new-character-form input[name=weapon]").value
+    let newCharacter = { 
+      name: name,
+      occupation: occupation,
+      cartoon: cartoon,
+      weapon: weapon,
+    }
+    charactersAPI.createOneRegister(newCharacter)          
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('edit-character-form').onsubmit = function(){
+    let id = document.querySelector('#edit-character-form input[name=chr-id]').value 
+    let name = document.querySelector('#edit-character-form input[name=name]').value
+    let occupation = document.querySelector('#edit-character-form input[name=occupation]').value
+    let weapon = document.querySelector('#edit-character-form input[name=weapon]').value
+    let cartoon = document.querySelector('#edit-character-form input[name=cartoon]').checked
+    let editCharacter = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon,
+    }
+    charactersAPI.updateOneRegister(id,editCharacter)
+  }
+
+  function addTableRows(data) {
+    for (let i = 0; i < data.length; i++) {
+      document.getElementById('character-container').innerHTML +=
+      `<div class="character-info">
+        <div class="name">Name: ${data[i].name}</div>
+        <div class="occupation">Occupation:${data[i].occupation}</div>
+        <div class="cartoon">Cartoon: ${data[i].cartoon}</div>
+        <div class="weapon">Weapon: ${data[i].weapon}</div>
+      </div>`
+    }
   }
 })
